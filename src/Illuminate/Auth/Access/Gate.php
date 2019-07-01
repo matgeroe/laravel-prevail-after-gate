6cbca98d851b9dccb01a7799aa11a2d62d8ddda5
Matthias Wilbrink <m.wilbrink@hanze-ict.nl>
2019-07-01 12:04:31 +0200

    init

---
+++ src/Illuminate/Auth/Access/Gate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MatthiasWilbrink\AfterGate\Illuminate\Auth\Access;
+
+use Illuminate\Auth\Access\Gate as IlluminateGate;
+
+class Gate extends IlluminateGate
+{
+    /**
+     * Call all of the after callbacks with check result.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable $user
+     * @param  string $ability
+     * @param  array $arguments
+     * @param  bool $result
+     * @return bool|null
+     */
+    protected function callAfterCallbacks($user, $ability, array $arguments, $result)
+    {
+        foreach ($this->afterCallbacks as $after) {
+            if (! $this->canBeCalledWithUser($user, $after)) {
+                continue;
+            }
+
+            $afterResult = $after($user, $ability, $result, $arguments);
+
+            $result = $afterResult ?? $result;
+        }
+
+        return $result;
+    }
+}
